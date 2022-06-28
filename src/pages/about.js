e229304813879be7ed9ab39fef3c50b72f58e65e
nunusmohum <halojw301@gmail.com>
2022-06-28 13:57:32 +0900

    Layout.js 추가 및 반영, index와 about에 적용, 미사용 파일들 삭제

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,12 +1,14 @@
-import * as React from "react";
+import React from "react";
+import Layout from "../components/Layout";
 
 // markup
 const AboutPage = () => {
   return (
-    <main>
-      <title>자기소개</title>
-      <h1>자기소개 페이지 입니다.</h1>
-    </main>
+    <Layout pageTitle="About Me">
+      <p>
+        Hi there! I'm the proud creator of this site, which I built with Gatsby.
+      </p>
+    </Layout>
   );
 };
 
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,14 @@
-import * as React from "react";
+import React from "react";
+import Layout from "../components/Layout";
 
 // markup
 const IndexPage = () => {
   return (
     <main>
       <title>Home Page</title>
-      <h1>Home Page</h1>
+      <Layout pageTitle="Home Page">
+        <p>I'm making this by following the Gatsby Tutorial.</p>
+      </Layout>
     </main>
   );
 };
