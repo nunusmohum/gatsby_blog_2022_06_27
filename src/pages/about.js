77939dd88ff6c74a84b1de33a5149b733896ee77
nunusmohum <halojw301@gmail.com>
2022-06-28 10:08:58 +0900

    about, test, test-space 페이지 추가

---
+++ src/pages/about.js
@@ -0,0 +1,13 @@
+import * as React from "react";
+
+// markup
+const AboutPage = () => {
+  return (
+    <main>
+      <title>자기소개</title>
+      <h1>자기소개 페이지 입니다.</h1>
+    </main>
+  );
+};
+
+export default AboutPage;
---
+++ src/pages/test.js
@@ -0,0 +1,13 @@
+import * as React from "react";
+
+// markup
+const TestPage = () => {
+  return (
+    <main>
+      <title>테스트</title>
+      <h1>테스트 페이지 입니다.</h1>
+    </main>
+  );
+};
+
+export default TestPage;
